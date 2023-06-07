5fee9f95f2f0e22d2219db2afc9035bbc37c4dac
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-07 19:48:26 -0400

    g

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ const next = document.getElementById('next')
 const p1 = document.getElementById('p1')
 const p2 = document.getElementById('p2')
 const goReset = document.getElementById('goReset')
+
+const stateArray = [
+    { topText: "I can read your mind", nextButton: "", textBelowButton: "" },
+
+    { topText: "Pick a number from 01-99", nextButton: "NEXT", textBelowButton: "when you have your number click next" },
+
+    { namtopTexte: "Add both digits together to get a new number", nextButton: "NEXT", textBelowButton: "Ex: 14 is 1 + 4 = 5" },
+
+    { topText: "Subtract your new number from the original number", nextButton: "NEXT", textBelowButton: "Ex: 14 - 5 = 9" },
+
+    { topText: "symbols", nextButton: "REVEAL", textBelowButton: "Find your new number." },
+
+    { topText: "symbol", nextButton: "", textBelowButton: "Your symbol is:" }
+  ];
+  
