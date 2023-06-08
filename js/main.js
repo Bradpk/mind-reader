a57232ef339e30613cca24967e00058cd0a35013
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-08 16:18:19 -0400

    g

--- js/main.js
+++ js/main.js
@@ -7,29 +7,21 @@ const goReset = document.getElementById('goReset')
 let pageIndex = 0
 
 
-
-
-//--- Changes the text inside the goReset button to "Reset once clicked"
-const buttons = document.getElementById("goReset");
-buttons.addEventListener('click', () => {
-    buttons.textContent = 'Reset';
-    })
-
 /*--- Contains an array of objects that corrispond to different pages. Each page contains its own properties which will display 
 different html element when cycled through. 
 */
 const pageArray = [
-    { topText: "I can read your mind", nextButton: "", textBelowButton: "" },//page 1
+    { topText: "I can read your mind", nextButton: "", textBelowButton: "", goButton: "Go" },//page 1
 
-    { topText: "Pick a number from 01-99", nextButton: "NEXT", textBelowButton: "when you have your number click next" },//page 2
+    { topText: "Pick a number from 01-99", nextButton: "NEXT", textBelowButton: "when you have your number click next", goButton: "Reset" },//page 2
 
-    { namtopTexte: "Add both digits together to get a new number", nextButton: "NEXT", textBelowButton: "Ex: 14 is 1 + 4 = 5" },//page 3
+    { namtopTexte: "Add both digits together to get a new number", nextButton: "NEXT", textBelowButton: "Ex: 14 is 1 + 4 = 5", goButton: "Reset" },//page 3
 
-    { topText: "Subtract your new number from the original number", nextButton: "NEXT", textBelowButton: "Ex: 14 - 5 = 9" },//page 4
+    { topText: "Subtract your new number from the original number", nextButton: "NEXT", textBelowButton: "Ex: 14 - 5 = 9", goButton: "Reset" },//page 4
 
-    { topText: "symbols", nextButton: "REVEAL", textBelowButton: "Find your new number." },//page 5
+    { topText: "symbols", nextButton: "REVEAL", textBelowButton: "Find your new number.", goButton: "Reset" },//page 5
 
-    { topText: "symbol", nextButton: "", textBelowButton: "Your symbol is: &" }//page 6
+    { topText: "symbol", nextButton: "", textBelowButton: "Your symbol is: &", goButton: "Reset" }//page 6
   ];
 
 /*--- This function will cycle through the different object arrays (pages). 
@@ -44,6 +36,7 @@ let currentPage = pageArray[pageIndex]
 h1.innerText = currentPage.topText
 next.innerText = currentPage.nextButton
 p1.innerText = currentPage.textBelowButton
+goReset.innerText = currentPage.goButton
 pageIndex = (pageIndex + 1)
 }
 
@@ -64,16 +57,29 @@ function changePageReset() {
 /*--- Inserting the changePage function into the "goReset" button event listener will change the page upon clicking by displaying 
 the html element in the second object array.
 */
-goReset.addEventListener("click", changePageReset);
+
+//goReset.addEventListener("click", changePage);
 
 /*--- Inserting the changePage function into the "next" button event listener will change the page upon clicking by displaying 
 the html element in the second object array.
 */
+
 next.addEventListener("click", changePage);
 
 //--- Figuring out how to revert back to page one upon clicking the go reset button the second time
-function reset() {
-if (goReset.textContent === "Reset") {
-   
+
+let buttonCount = 0
+
+function clickCount(){
+    buttonCount = buttonCount + 1
+    if (buttonCount <= 1) {
+        return changePage()
+    } else {
+        return changePageReset()
+        buttonCount = 0
+        pageIndex = 0
+    }
 }
-}
+
+
+goReset.addEventListener("click", clickCount)
