ce09bf43ecf0cb6e89940dc85d10582a373494e6
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-08 14:35:47 -0400

    g

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//--- Creates the variables that are lniked with their html elements 
 const h1 = document.getElementById("h1")
 const next = document.getElementById('next')
 const p1 = document.getElementById('p1')
@@ -5,13 +6,15 @@ const p2 = document.getElementById('p2')
 const goReset = document.getElementById('goReset')
 
 
-//---
+//--- Changes the text inside the goReset button to "Reset once clicked"
 const buttons = document.getElementById("goReset");
 buttons.addEventListener('click', () => {
     buttons.textContent = 'Reset';
     })
 
-//---
+/*--- Contains an array of objects that corrispond to different pages. Each page contains its own properties which will display 
+different html element when cycled through. 
+*/
 const pageArray = [
     { topText: "I can read your mind", nextButton: "", textBelowButton: "" },//page 1
 
@@ -21,12 +24,17 @@ const pageArray = [
 
     { topText: "Subtract your new number from the original number", nextButton: "NEXT", textBelowButton: "Ex: 14 - 5 = 9" },//page 4
 
-    { topText: "symbol0s", nextButton: "REVEAL", textBelowButton: "Find your new number." },//page 5
+    { topText: "symbols", nextButton: "REVEAL", textBelowButton: "Find your new number." },//page 5
 
     { topText: "symbol", nextButton: "", textBelowButton: "Your symbol is: &" }//page 6
   ];
 
-//---
+/*--- This function will cycle through the different object arrays (pages). 
+currentPage is set to equal a different array number in pageArray
+.innerText will change the html content of the chosen variable by setting it to equal one of the properties in one of 
+the objects based upon which array number it is currently at. 
+pageIndex will increase the pageIndex of itself by one which will change the html display to the properties in the next array. 
+*/
 let pageIndex = 0
   
 function changePage() {
@@ -37,11 +45,17 @@ p1.innerText = currentPage.textBelowButton
 pageIndex = (pageIndex + 1)
 }
   
-//---
+/*--- Inserting the changePage function into the "goReset" button event listener will change the page upon clicking by displaying 
+the html element in the second object array.
+*/
 goReset.addEventListener("click", changePage);
+
+/*--- Inserting the changePage function into the "next" button event listener will change the page upon clicking by displaying 
+the html element in the second object array.
+*/
 next.addEventListener("click", changePage);
 
-//---
+//--- Figuring out how to revert back to page one upon clicking the go reset button the second time
 function reset() {
 if (goReset.textContent === "Reset") {
    
